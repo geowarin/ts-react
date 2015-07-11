d367d72cfcc1ec1555bdca36edda79b561ff7343
geowarin <geowarin@gmail.com>
2015-07-11 19:44:16 +0200

    Use [tsd](https://github.com/DefinitelyTyped/tsd) to manage typings

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+/// <reference path="../typings/tsd.d.ts" />
+
 import React = require('react');
 import App from './App';
 
---
+++ typings/tsd.d.ts
@@ -0,0 +1,3 @@
+
+/// <reference path="react/react.d.ts" />
+/// <reference path="react/react-jsx.d.ts" />
